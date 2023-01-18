b50f644bfdab09108656ec78c74904dfdfb10cbb
Vasco Dias <m+git@vascorsd.com>
2023-01-18 03:09:17 +0000

    surprisingly, it runs

--- Main.scala
+++ Main.scala
@@ -1,4 +1,37 @@
+import org.http4s.server.Server
 @main
-def run(): Unit = {
+def test(): Unit = {
   println("test")
+}
+
+@main
+def server(): Unit = {
+  import cats.effect._, org.http4s._, org.http4s.dsl.io._
+  import cats.effect.unsafe.IORuntime
+  implicit val runtime: IORuntime = cats.effect.unsafe.IORuntime.global
+
+import cats.syntax.all._
+import com.comcast.ip4s._
+import org.http4s.ember.server._
+import org.http4s.implicits._
+import org.http4s.server.Router
+import scala.concurrent.duration._
+    
+  val helloWorldService = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name.")
+  }
+
+  val httpApp = Router("/" -> helloWorldService).orNotFound
+
+  val server: Resource[cats.effect.IO, Server] = EmberServerBuilder
+    .default[IO]
+    .withHost(ipv4"0.0.0.0")
+    .withPort(port"8080")
+    .withHttpApp(httpApp)
+    .build
+
+  val shutdown = server.allocated.unsafeRunSync()._2
+
+      while (true) {}
 }
